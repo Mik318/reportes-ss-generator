42618b5ed9a9681c02dad2b26b8de98c185657a4
mik318 <lokilskdij@gmail.com>
2025-09-14 19:01:05 -0600

    feat: Add configuracion page and update routing to include new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '**',
-    redirectTo: 'home'
+    path: 'home',
+    loadComponent: () => import('./pages/home/home')
+      .then(m => m.Home)
   },
   {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    path: 'configuracion',
+    loadComponent: () => import('./pages/configuracion/configuracion')
+      .then(m => m.Configuracion)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
   }
 ];
