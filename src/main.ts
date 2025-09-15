05fbfa48ab65417d11cc46cff70bb655dcdd93be
mik318 <lokilskdij@gmail.com>
2025-09-14 18:42:06 -0600

    refactor: Update routing configuration to use HashLocationStrategy and enable wildcard redirect to home

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+// main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { routes } from './app/app.routes';
+import {App} from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+});
