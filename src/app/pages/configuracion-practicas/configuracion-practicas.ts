32e094a91eafed96c8dd23d4ddbe952d7441ac6d
Juan gomen <gomen98.jg@gmail.com>
2025-09-24 22:49:27 -0600

    feat: add initial scaffolding for practicas configuration page

---
+++ src/app/pages/configuracion-practicas/configuracion-practicas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracion-practicas',
+  imports: [],
+  templateUrl: './configuracion-practicas.html',
+  styleUrl: './configuracion-practicas.css'
+})
+export class ConfiguracionPracticas {
+
+}
